825ef4857be52eefba0348854d381b5aab60f8a6
Lucas Ferraço <lucasferracof@gmail.com>
2018-08-08 15:17:42 -0300

    Adding web interface

--- EmojiJournalServer/Sources/EmojiJournalServer/Routes/WebClientRoutes.swift
+++ EmojiJournalServer/Sources/EmojiJournalServer/Routes/WebClientRoutes.swift
@@ -9,22 +9,63 @@ import Foundation
 import LoggerAPI
 import KituraStencil
 import Kitura
+import CouchDB
+
+private var database: Database?
 
 func initializeWebClientRoutes(app: App) {
     app.router.setDefault(templateEngine: StencilTemplateEngine())
     app.router.all(middleware: StaticFileServer(path: "./public"))
     app.router.get("/client", handler: showClient)
+    database = app.database
     
     Log.info("Web client routes created")
 }
 
 func showClient(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) {
-    var context = [String : Any]()
-    context["title"] = "I'm a web developer now!"
+    guard let database = database else {
+        response.status(.serviceUnavailable).send(json: ["Message": "CouchDB Unavailable"])
+        return
+    }
     
-    do {
-        try response.render("home.stencil", context: context)
-    } catch let error {
-        response.status(.internalServerError).send(error.localizedDescription)
+    JournalEntry.Persistence.getAll(for: database) { (entries, error) in
+        guard let entries = entries else {
+            response.status(.serviceUnavailable).send(json: ["Message": error?.localizedDescription])
+            return
+        }
+        
+        let sortedEntries = entries.sorted(by: { (entry1, entry2) -> Bool in
+            entry1.date.timeIntervalSince1970 > entry2.date.timeIntervalSince1970
+        })
+        
+        var context = [String : Any]()
+        var contextEntries = [[String : Any]]()
+        for entry in sortedEntries {
+            if let id = entry.id {
+                let map = [
+                    "emoji": entry.emoji,
+                    "date": entry.date.displayDate,
+                    "id": id,
+                    "time": entry.date.displayTime,
+                    "emojiBGColor": entry.backgroundColorCode
+                ]
+                
+                contextEntries.append(map)
+            }
+        }
+        
+        context["entries"] = contextEntries
+        do {
+            try response.render("home.stencil", context: context)
+        } catch let error {
+            response.status(.internalServerError).send(error.localizedDescription)
+        }
+    }
+}
+
+fileprivate extension JournalEntry {
+    var backgroundColorCode: String {
+        guard let substring = id?.suffix(6).uppercased() else { return "000000" }
+        return substring
     }
 }
